49fe6c7f22921d5118410c62ba17f04c1b7447ea
Robin Guyton <GladiatorRobin@aol.com>
2025-04-06 15:24:27 -0700

    Tested links

---
+++ COMP003B.LectureActivity4/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.LectureActivity4.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Summary { get; set; }
+    }
+}
